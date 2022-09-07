54da1079ba2d06e6cb4c13d05a59c965e5f8e83b
mj <majestyyolo@gmail.com>
2022-09-07 10:04:19 -0700

    first

---
+++ script.js
@@ -0,0 +1,71 @@
+  // Item One
+  let item_1 = {name: 'Bread', quantity: 2, price: 500};
+  let item_1_total = item_1.quantity * item_1.price;
+
+  document.getElementById('first_item_description').innerHTML = item_1.name;
+  document.getElementById('first_item_quantity').innerHTML = item_1.quantity;
+  document.getElementById('first_item_price').innerHTML = item_1.price;
+  document.getElementById('first_item_total').innerHTML = item_1_total;
+  // Item One
+
+  // Item Two
+  let item_2 = {name: 'Bag of rice', quantity: 3, price: 25000};
+  let item_2_total = item_2.quantity * item_2.price;
+
+  document.getElementById('second_item_description').innerHTML = item_2.name;
+  document.getElementById('second_item_quantity').innerHTML = item_2.quantity;
+  document.getElementById('second_item_price').innerHTML = item_2.price;
+  document.getElementById('second_item_total').innerHTML = item_2_total;
+  // Item Two
+
+  // Item Three
+  let item_3 = {name: 'Milo (30kg)', quantity: 5, price: 1700};
+  let item_3_total = item_3.quantity * item_3.price;
+
+  document.getElementById('third_item_description').innerHTML = item_3.name;
+  document.getElementById('third_item_quantity').innerHTML = item_3.quantity;
+  document.getElementById('third_item_price').innerHTML = item_3.price;
+  document.getElementById('third_item_total').innerHTML = item_3_total;
+  // Item Three
+
+  // Item Four
+  let item_4 = {name: 'Semovita', quantity: 7, price: 2500};
+  let item_4_total = item_4.quantity * item_4.price;
+
+  document.getElementById('fourth_item_description').innerHTML = item_4.name;
+  document.getElementById('fourth_item_quantity').innerHTML = item_4.quantity;
+  document.getElementById('fourth_item_price').innerHTML = item_4.price;
+  document.getElementById('fourth_item_total').innerHTML = item_4_total;
+  // Item Four
+
+  // Item Five
+  let item_5 = {name: 'Sphagetti', quantity: 12, price: 500};
+  let item_5_total = item_5.quantity * item_5.price;
+
+  document.getElementById('fifth_item_description').innerHTML = item_5.name;
+  document.getElementById('fifth_item_quantity').innerHTML = item_5.quantity;
+  document.getElementById('fifth_item_price').innerHTML = item_5.price;
+  document.getElementById('fifth_item_total').innerHTML = item_5_total;
+  // Item Five
+
+  // Item Six
+  let item_6 = {name: 'Bag of Beans', quantity: 2, price: 15000};
+  let item_6_total = item_6.quantity * item_6.price;
+
+  document.getElementById('sixth_item_description').innerHTML = item_6.name;
+  document.getElementById('sixth_item_quantity').innerHTML = item_6.quantity;
+  document.getElementById('sixth_item_price').innerHTML = item_6.price;
+  document.getElementById('sixth_item_total').innerHTML = item_6_total;
+  // Item Six
+
+  // Item Seven
+  let item_7 = {name: 'Pack of Maggi', quantity: 5, price: 1200};
+  let item_7_total = item_7.quantity * item_7.price;
+
+  document.getElementById('seventh_item_description').innerHTML = item_7.name;
+  document.getElementById('seventh_item_quantity').innerHTML = item_7.quantity;
+  document.getElementById('seventh_item_price').innerHTML = item_7.price;
+  document.getElementById('seventh_item_total').innerHTML = item_7_total;
+  // Item Seven
+
+  grand_total = item_1_total + item_2_total + item_3_total + item_4_total + item_5_total + item_6_total + item_7_total;
